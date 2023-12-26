8cf1e037dec17b2a686f640c127a7792e02a5255
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-12-26 19:28:52 -0300

    feat(web):add type of collection in the code folder

---
+++ code/type-of-collection.ts
@@ -0,0 +1,11 @@
+// -------- Arrays -------- //
+
+const arrayNumber: Array<number> = [1, 2, 3];
+const arrayString: Array<string> = ["one", "two", "three"];
+
+const number: number[] = [1, 2, 3];
+const string: string[] = ["one", "two", "three"];
+
+// -------- Tuples -------- //
+
+const tuples: [string, number] = ["Andres", 24];
