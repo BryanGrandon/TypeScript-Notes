f2c05f3836d7eaa8df746436f643c8a7fd2ec160
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-12-27 19:47:56 -0300

    feat(web):add modules in the code folder

---
+++ code/modules/import.ts
@@ -0,0 +1,11 @@
+import { Person, getNumberRandom } from "./export";
+import { PI as myPI } from "./export";
+
+const user: Person = {
+  id: 2,
+  name: "Juan",
+};
+
+console.log(myPI);
+console.log(user);
+console.log(getNumberRandom());
