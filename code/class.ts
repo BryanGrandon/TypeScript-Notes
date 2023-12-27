76ec4ab2b002f40e90bde1601c71908026123102
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-12-27 19:02:45 -0300

    feat(web):add class in the code folder

---
+++ code/class.ts
@@ -0,0 +1,77 @@
+class Car {
+  // Properties
+  private _color: string;
+  private _make: string;
+  private _doors: number;
+
+  // Constructor
+  constructor(make: string, color: string, doors = 4) {
+    this._make = make;
+    this._color = color;
+    if (doors % 2 === 0) {
+      this._doors = doors;
+    } else {
+      throw new Error(`Doors must be an even number`);
+    }
+  }
+
+  // Accessors
+  get color() {
+    return `The color of the car is ` + this._color;
+  }
+  set color(color) {
+    this._color = color;
+  }
+  get doors() {
+    return this._doors;
+  }
+
+  // Methods
+  accelerate(speed: number): string {
+    return `${this.worker()} is accelerating to ${speed} MPH`;
+  }
+  turn(direction: "left" | "right"): string {
+    return `${this.worker()} is turning ${direction}`;
+  }
+  worker(): string {
+    return this._make;
+  }
+}
+
+let myCar1 = new Car("Cool Car Company", "blue", 2);
+
+console.log(myCar1.accelerate(35));
+console.log(myCar1.turn("right"));
+
+let myCar2 = new Car("Galaxy Motors", "red");
+console.log(myCar2.doors);
+
+// Expansion of a class
+class ElectricCar extends Car {
+  private _range: number;
+  constructor(make: string, color: string, range: number, doors = 2) {
+    super(make, color, doors);
+    this._range = range;
+  }
+  get range() {
+    return this._range;
+  }
+  set range(range) {
+    this._range = range;
+  }
+  charge() {
+    console.log(this.worker() + " is charging.");
+  }
+}
+
+let spark = new ElectricCar("Spark Motors", "silver", 124, 2);
+let eCar = new ElectricCar("Electric Car Co.", "black", 263);
+console.log(eCar.doors); // returns the default, 2
+spark.charge(); // returns "Spark Motors is charging"
+
+/*  Access control keywords
+    - Public    -> if you do not specify an access modifier, the default is public.
+    - Private   -> If you modify the member with the private keyword, it cannot be accessed from outside the containing class.
+    - Protected -> It is similar to the private modifier, with the difference that it can access protected elements within derived classes.
+    - Readonly  -> Read-only modifier
+*/
